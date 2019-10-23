eaf551770c5b845658a4ffbc2dc516aab3813404
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-22 21:20:24 -0500

    update script to only push for directories

--- push.sh
+++ push.sh
@@ -4,15 +4,17 @@ adb shell mkdir -p /sdcard/media/audio/alarms
 adb shell mkdir -p /sdcard/media/audio/notifications
 adb shell mkdir -p /sdcard/media/audio/ringtones
 
-for f in `ls | grep -v push.sh`; do
-  cd $f
-  cd alarms
-  adb push . /sdcard/media/audio/alarms/
-  cd ../notifications
-  adb push . /sdcard/media/audio/notifications
-  cd ../ringtones
-  adb push . /sdcard/media/audio/ringtones
-  cd ../..
+for f in *; do
+  if [ -d $f ]; then
+    cd $f
+    cd alarms
+    adb push . /sdcard/media/audio/alarms/
+    cd ../notifications
+    adb push . /sdcard/media/audio/notifications
+    cd ../ringtones
+    adb push . /sdcard/media/audio/ringtones
+    cd ../..
+  fi
 done
 
 echo ""
